4852a8eb858afefe781df07a9eb2743ff812e464
Empa Emanuel <empaemanuel@gmail.com>
2019-11-11 12:56:07 +0100

    Added skeleton node-classes

---
+++ src/main/java/PROP_0/AssignmentNode.java
@@ -0,0 +1,11 @@
+package main.java.PROP_0;
+
+public class AssignmentNode implements INode {
+    public Object evaluate(Object[] args) throws Exception {
+        return null;
+    }
+
+    public void buildString(StringBuilder builder, int tabs) {
+
+    }
+}
---
+++ src/main/java/PROP_0/Evaluator.java
@@ -0,0 +1,4 @@
+package main.java.PROP_0;
+
+public class Evaluator {
+}
