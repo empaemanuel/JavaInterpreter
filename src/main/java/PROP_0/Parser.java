ca4eaeeb9db6fb8cf653ec8dfb5c03282b2ffdab
Empa Emanuel <empaemanuel@gmail.com>
2019-11-11 11:28:15 +0100

    initial project skeleton

---
+++ src/main/java/PROP_0/Parser.java
@@ -0,0 +1,20 @@
+package main.java.PROP_0;
+
+import java.io.IOException;
+
+public class Parser implements IParser {
+    //Den här klassen ska vi skriva
+    public void open(String fileName) throws IOException, TokenizerException {
+
+    }
+
+    public INode parse() throws IOException, TokenizerException, ParserException {
+        return null;
+    }
+
+    public void close() throws IOException {
+
+    }
+
+
+}
---
+++ src/main/java/PROP_0/TokenizerException.java
@@ -0,0 +1,7 @@
+package main.java.PROP_0;
+
+public class TokenizerException extends Exception {
+	public TokenizerException(String message) {
+		super(message);
+	}
+}
