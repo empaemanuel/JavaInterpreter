2a5c3cf67770403d6bebbc9ab506bbf49062fff6
Empa Emanuel <empaemanuel@gmail.com>
2019-11-12 10:23:53 +0100

    added som comments to readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PROP_0
 A simple Tokenizer and Parser in Java.
+
+sem.java.SEM1 innehåller kod från seminariet på liknande uppgift.
+Vi kan använda denna som referens.
+
+Kodskellettet är klart. Ej klart om evaluator klassen ska vara en egen klass eller ej.
+Det kommer vi att kunna hantera i slutet på uppgiften.
+
+Det är Lexeme, Parser, Tokenizer och Evaluator som vi ska skriva.
+
+Input textfilerna kan komma att behöva flyttas in i main.java.prop_0 paketet.
+
+Inga associationer mellan klasser gjorda för tillfället..
+
+
+
--- src/main/java/PROP_0/Tokenizer.java
+++ src/main/java/PROP_0/Tokenizer.java
@@ -3,11 +3,9 @@ package main.java.PROP_0;
 import java.io.IOException;
 
 public class Tokenizer implements ITokenizer{
-    //Denna ska vi skriva
-    //Börjar med pseudo
 
     public void open(String fileName) throws IOException, TokenizerException {
-        //öppna scannern
+
     }
 
     public Lexeme current() {
@@ -15,10 +13,6 @@ public class Tokenizer implements ITokenizer{
     }
 
     public void moveNext() throws IOException, TokenizerException {
-        //OM next inte är EOL
-        //Consume whitespace
-        //
-        //Gå till nästa char, consume whitespace, EOL, SOL.
 
     }
 
