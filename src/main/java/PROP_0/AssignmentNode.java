654aaf44fe288400019e844ae65b76e541c1b2a0
Thing Thinge <thing@something.com>
2019-11-13 13:52:23 +0100

    Early tokenizer implementation, added node skeleton

---
+++ src/main/java/PROP_0/AssignmentNode.java
@@ -0,0 +1,13 @@
+package main.java.PROP_0;
+
+public class AssignmentNode implements INode {
+    @Override
+    public Object evaluate(Object[] args) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void buildString(StringBuilder builder, int tabs) {
+
+    }
+}
--- src/main/java/PROP_0/Tokenizer.java
+++ src/main/java/PROP_0/Tokenizer.java
@@ -2,18 +2,132 @@ package main.java.PROP_0;
 
 import java.io.IOException;
 
-public class Tokenizer implements ITokenizer{
+public class Tokenizer implements ITokenizer {
+    Scanner scanner = new Scanner();
+    Lexeme current = null;
+
     //Denna ska vi skriva
     public void open(String fileName) throws IOException, TokenizerException {
 
     }
 
     public Lexeme current() {
-        return null;
+        return current;
     }
 
     public void moveNext() throws IOException, TokenizerException {
+        getNonBlank();
+        try {
+            current = buildLex();
+        } catch (TokenizerException e) {
 
+        }
+    }
+
+    private void getNonBlank() {
+        try {
+            while (Character.isWhitespace(scanner.current())) {
+                scanner.moveNext();
+            }
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    private Lexeme buildLex() throws TokenizerException {
+        char currentChar = scanner.current();
+
+        if (Character.isLetter(currentChar)) {
+            return buildIdentifier();
+        }
+        if (Character.isDigit(currentChar)) {
+            return buildIntLiteral();
+        }
+        if (charIsOperator(currentChar)) {
+            return identifyOperator(currentChar);
+        }
+        if (currentChar == '('){
+            return new Lexeme(currentChar,Token.LEFT_PAREN);
+        }
+        if (currentChar == ')'){
+            return new Lexeme(currentChar, Token.RIGHT_PAREN);
+        }
+        if (currentChar == '{'){
+            return new Lexeme(currentChar,Token.LEFT_CURLY);
+        }
+        if(currentChar == '}'){
+            return  new Lexeme(currentChar,Token.RIGHT_CURLY);
+        }
+
+        return null;
+
+    }
+
+    private Lexeme buildIdentifier() throws TokenizerException {
+        try {
+            char currentChar = scanner.current();
+            StringBuilder identValue = new StringBuilder();
+            while (Character.isLetter(currentChar)) {
+                identValue.append(currentChar);
+                scanner.moveNext();
+                currentChar = scanner.current();
+            }
+            if (currentChar == '=' || Character.isWhitespace(currentChar)) {
+                return new Lexeme(identValue, Token.IDENT);
+            }
+        } catch (IOException e) {
+
+        }
+        throw new TokenizerException("Illegal character combination in stream");
+    }
+
+    private Lexeme buildIntLiteral() throws TokenizerException {
+        try {
+            char currentChar = scanner.current();
+            StringBuilder identValue = new StringBuilder();
+            while (Character.isDigit(currentChar)) {
+                identValue.append(currentChar);
+                scanner.moveNext();
+                currentChar = scanner.current();
+            }
+            if (charIsOperator(currentChar) || Character.isWhitespace(currentChar)) {
+                return new Lexeme(Integer.valueOf(identValue.toString()), Token.INT_LIT);
+            }
+        } catch (IOException e) {
+
+        }
+        throw new TokenizerException("Illegal character combination in stream");
+    }
+
+    private Lexeme identifyOperator(char currentChar) {
+        if (currentChar == '+') {
+            return new Lexeme(currentChar, Token.ADD_OP);
+        }
+        if (currentChar == '-') {
+            return new Lexeme(currentChar, Token.SUB_OP);
+        }
+        if (currentChar == '*') {
+            return new Lexeme(currentChar, Token.MULT_OP);
+        }
+
+        return new Lexeme(currentChar, Token.DIV_OP);
+
+
+    }
+/*
+    private boolean charIsParanthesis(char in) {
+        if (in == '(' || in == ')') {
+            return true;
+        }
+        return false;
+    }
+*/
+    private boolean charIsOperator(char in) {
+        if (in == '/' || in == '*' || in == '+' || in == '-') {
+            return true;
+        }
+        return false;
     }
 
     public void close() throws IOException {
